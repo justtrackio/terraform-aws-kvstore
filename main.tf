305c25a0f03143c86abb3775693c51a0b001d2c6
Sebastian Struß <31846129+applike-ss@users.noreply.github.com>
2022-12-15 13:11:18 +0100

    feat: Added functionality (#1)
    ## Description
    <!--- Describe your changes in detail -->
    
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## Breaking Changes
    <!-- Does this break backwards compatibility with the current major
    version? -->
    <!-- If so, please provide an explanation why it is necessary. -->
    
    ## How Has This Been Tested?
    - [ ] I have updated at least one of the `examples/*` to demonstrate and
    validate my change(s)
    - [ ] I have tested and validated these changes using one or more of the
    provided `examples/*` projects
    <!--- Users should start with an existing example as its written, deploy
    it, then check their changes against it -->
    <!--- This will highlight breaking/disruptive changes. Once you have
    checked, deploy your changes to verify -->
    <!--- Please describe how you tested your changes -->
    - [ ] I have executed `pre-commit run -a` on my pull request
    <!--- Please see
    https://github.com/antonbabenko/pre-commit-terraform#how-to-install for
    how to install -->

---
+++ main.tf
@@ -0,0 +1,139 @@
+locals {
+  redis_container_name = "redis"
+}
+
+# can't just pull in the context here because the attributes add up instead of being replaced by the input
+module "ddb_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
+  enabled             = module.this.enabled
+  namespace           = module.this.namespace
+  tenant              = module.this.tenant
+  environment         = module.this.environment
+  stage               = module.this.stage
+  name                = module.this.name
+  delimiter           = module.this.delimiter
+  tags                = module.this.tags
+  additional_tag_map  = module.this.additional_tag_map
+  label_order         = var.ddb_label_order
+  regex_replace_chars = module.this.regex_replace_chars
+  id_length_limit     = module.this.id_length_limit
+  label_key_case      = var.label_key_case
+  label_value_case    = var.label_value_case
+  descriptor_formats  = var.descriptor_formats
+  labels_as_tags      = var.labels_as_tags
+}
+
+module "redis_label" {
+  count   = var.use_redis ? 1 : 0
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
+  context     = module.ddb_label.context
+  label_order = var.redis_label_order
+}
+
+module "ddb" {
+  source = "github.com/justtrackio/terraform-aws-dynamodb-table?ref=v1.0.0"
+
+  context    = module.ddb_label.context
+  attributes = ["kvstore-${join("", var.attributes)}"]
+
+  billing_mode = var.ddb_billing_mode
+
+  autoscale_read_schedule  = var.ddb_autoscale_read_schedule
+  autoscale_write_schedule = var.ddb_autoscale_write_schedule
+
+  hash_key = "key"
+
+  dynamodb_attributes = [{
+    name = "key"
+    type = "S"
+  }]
+
+  ttl_enabled = false
+
+  tags = {
+    Model = "kvstore_${join("", var.attributes)}"
+  }
+}
+
+module "container_definition" {
+  count   = var.use_redis ? 1 : 0
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.58.1"
+
+  container_image              = "${var.redis_image_repository}:${var.redis_image_tag}"
+  container_name               = local.redis_container_name
+  container_cpu                = var.redis_cpu_size
+  container_memory_reservation = var.redis_service_memory_size
+
+  port_mappings = [
+    {
+      containerPort = 6379
+      hostPort      = 0
+      protocol      = "tcp"
+    },
+  ]
+
+  command = [
+    "--maxmemory ${var.redis_memory_size}mb",
+    "--maxmemory-policy allkeys-lru"
+  ]
+}
+
+module "redis" {
+  count   = var.use_redis ? 1 : 0
+  source  = "cloudposse/ecs-alb-service-task/aws"
+  version = "0.66.4"
+
+  context = module.redis_label[0].context
+
+  container_definition_json          = "[${sensitive(module.container_definition[0].json_map_encoded)}]"
+  deployment_maximum_percent         = var.redis_deployment_maximum_percent
+  deployment_minimum_healthy_percent = var.redis_deployment_minimum_healthy_percent
+  desired_count                      = var.redis_desired_count
+  ecs_cluster_arn                    = var.redis_ecs_cluster_arn
+  launch_type                        = var.redis_launch_type
+  name                               = "${module.redis_label[0].id}-redis-kvstore_${join("", var.attributes)}"
+  network_mode                       = var.redis_network_mode
+  propagate_tags                     = var.redis_propagate_tags
+  vpc_id                             = var.redis_vpc_id
+
+  service_registries = [{
+    registry_arn   = aws_service_discovery_service.this[0].arn
+    container_name = local.redis_container_name
+    container_port = 6379
+  }]
+
+  tags = {
+    Model           = "kvstore_${join("", var.attributes)}"
+    ApplicationType = "redis"
+  }
+
+  service_placement_constraints = var.redis_service_placement_constraints != null ? var.redis_service_placement_constraints : module.this.environment == "prod" ? [{
+    type       = "memberOf"
+    expression = "attribute:spotinst.io/container-instance-lifecycle==od"
+  }] : []
+}
+
+resource "aws_service_discovery_service" "this" {
+  count = var.use_redis ? 1 : 0
+  name  = "kvstore_${join("", var.attributes)}.${module.this.name}.redis"
+
+  dns_config {
+    namespace_id = var.redis_service_discovery_dns_namespace_id
+
+    dns_records {
+      ttl  = 60
+      type = "SRV"
+    }
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+
+  tags = module.this.tags
+}
