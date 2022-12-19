d832c133105f8cbcd4a131fcde168d05638a547c
Sebastian Struß <31846129+applike-ss@users.noreply.github.com>
2022-12-19 10:52:43 +0100

    feat: Added ability to adjust label order for iam roles (#3)
    ## Description
    <!--- Describe your changes in detail -->
    
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## Breaking Changes
    <!-- Does this break backwards compatibility with the current major
    version? -->
    <!-- If so, please provide an explanation why it is necessary. -->
    
    ## How Has This Been Tested?
    - [ ] I have updated at least one of the `examples/*` to demonstrate and
    validate my change(s)
    - [ ] I have tested and validated these changes using one or more of the
    provided `examples/*` projects
    <!--- Users should start with an existing example as its written, deploy
    it, then check their changes against it -->
    <!--- This will highlight breaking/disruptive changes. Once you have
    checked, deploy your changes to verify -->
    <!--- Please describe how you tested your changes -->
    - [ ] I have executed `pre-commit run -a` on my pull request
    <!--- Please see
    https://github.com/antonbabenko/pre-commit-terraform#how-to-install for
    how to install -->

--- examples/all-labels/main.tf
+++ examples/all-labels/main.tf
@@ -7,6 +7,9 @@ module "example" {
   stage                                    = "st"
   name                                     = "nm"
   attributes                               = ["a1", "a2"]
+  ddb_label_order                          = ["environment", "name", "attributes"]
+  iam_role_label_order                     = ["environment", "name", "attributes"]
+  redis_label_order                        = ["name", "attributes"]
   label_value_case                         = "none" # only for keeping case
   redis_ecs_cluster_arn                    = "arn:aws:ecs:eu-central-1:123456789123:cluster/my-cluster"
   redis_service_discovery_dns_namespace_id = "ns-ab12c34defghij5k"
--- variables.tf
+++ variables.tf
@@ -34,6 +34,16 @@ variable "ddb_label_order" {
     EOT
 }
 
+variable "iam_role_label_order" {
+  type        = list(string)
+  default     = null
+  description = <<-EOT
+    The order in which the labels (ID elements) appear in the `id`.
+    Defaults to ["namespace", "environment", "stage", "name", "attributes"].
+    You can omit any of the 6 labels ("tenant" is the 6th), but at least one must be present.
+    EOT
+}
+
 variable "redis_cpu_size" {
   type        = string
   default     = 25
