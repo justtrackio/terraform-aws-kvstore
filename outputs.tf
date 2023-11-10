f3d0405b09647a14f974a4b6efc1ed8b969b5770
Sebastian Struß <31846129+applike-ss@users.noreply.github.com>
2023-11-10 07:45:18 +0100

    fix: fixed output when redis not used (#49)
    ## Description
    <!--- Describe your changes in detail -->
    
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## Breaking Changes
    <!-- Does this break backwards compatibility with the current major
    version? -->
    <!-- If so, please provide an explanation why it is necessary. -->
    
    ## How Has This Been Tested?
    - [ ] I have updated at least one of the `examples/*` to demonstrate and
    validate my change(s)
    - [ ] I have tested and validated these changes using one or more of the
    provided `examples/*` projects
    <!--- Users should start with an existing example as its written, deploy
    it, then check their changes against it -->
    <!--- This will highlight breaking/disruptive changes. Once you have
    checked, deploy your changes to verify -->
    <!--- Please describe how you tested your changes -->
    - [ ] I have executed `pre-commit run -a` on my pull request
    <!--- Please see
    https://github.com/antonbabenko/pre-commit-terraform#how-to-install for
    how to install -->

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "redis_service_discovery_service_arn" {
   description = "ARN of the aws_service_discovery_service created for the redis service"
-  value       = try(module.redis[0].service_discovery_service_arn)
+  value       = try(module.redis[0].service_discovery_service_arn, "")
 }
